b267cae9e3548ee882ed9a4cd83c0e3098131318
Richika Rana <richikarana18@gmail.com>
2026-04-21 01:05:18 +0530

    refactor: move ALLOWED_EXTENSIONS and file size limits out of config into validation layer

--- talkingdb/helpers/client.py
+++ talkingdb/helpers/client.py
@@ -6,10 +6,6 @@ load_dotenv()
 
 CLIENT_MODE = os.getenv("CLIENT_MODE", ClientMode.DIRECT)
 CE_HOST = os.getenv("CE_HOST", None)
-
-
-ALLOWED_EXTENSIONS = {"docx"}
-MAX_FILE_SIZE_MB = int(os.getenv("TDB_MAX_FILE_SIZE_MB", "50"))
 TDB_API_KEY = os.getenv("TDB_API_KEY")
 
 
@@ -17,9 +13,6 @@ class Config:
     CLIENT_MODE: ClientMode = CLIENT_MODE
     API_TIMEOUT = 300
     CE_HOST = CE_HOST
-    ALLOWED_EXTENSIONS = ALLOWED_EXTENSIONS
-    MAX_FILE_SIZE_MB = MAX_FILE_SIZE_MB
-    MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024
     TDB_API_KEY = TDB_API_KEY
 
 
